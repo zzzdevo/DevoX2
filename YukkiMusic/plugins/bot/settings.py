0fb91221418c0f6ee0480c11f337f739b3b94cab
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-10-03 12:16:27 +0300

    Update settings.py

--- YukkiMusic/plugins/bot/settings.py
+++ YukkiMusic/plugins/bot/settings.py
@@ -384,7 +384,7 @@ async def playmode_ans(client, CallbackQuery, _):
             Playtype = False
         else:
             await set_playtype(
-                CallbackQuery.message.chat.id, "Admin"
+                CallbackQuery.message.chat.id, "Everyone"
             )
             Playtype = True
         playmode = await get_playmode(CallbackQuery.message.chat.id)
