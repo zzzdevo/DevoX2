4438bcbabb2f06fafe3d1beba72022597a4f22f4
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-10-04 09:43:15 +0300

    Update speed.py

--- YukkiMusic/plugins/admins/speed.py
+++ YukkiMusic/plugins/admins/speed.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 from strings.filters import command
 from YukkiMusic import app
-from YukkiMusic.core.call import Yuki
+from YukkiMusic.core.call import Yukki
 from YukkiMusic.misc import SUDOERS, db
 from YukkiMusic.utils import AdminRightsCheck
 from YukkiMusic.utils.database import is_active_chat, is_nonadmin_chat
@@ -25,13 +25,13 @@ async def playback(cli, message: Message, _, chat_id):
         return await message.reply_text(_["queue_2"])
     duration_seconds = int(playing[0]["seconds"])
     if duration_seconds == 0:
-        return await message.reply_text(_["admin_27"])
+        return await message.reply_text(_["admin_35"])
     file_path = playing[0]["file"]
     if "downloads" not in file_path:
-        return await message.reply_text(_["admin_27"])
+        return await message.reply_text(_["admin_35"])
     upl = speed_markup(_, chat_id)
     return await message.reply_text(
-        text=_["admin_28"].format(app.mention),
+        text=_["admin_36"].format(app.mention),
         reply_markup=upl,
     )
 
@@ -44,57 +44,57 @@ async def del_back_playlist(client, CallbackQuery, _):
     chat, speed = callback_request.split("|")
     chat_id = int(chat)
     if not await is_active_chat(chat_id):
-        return await CallbackQuery.answer(_["general_5"], show_alert=True)
+        return await CallbackQuery.answer(_["general_6"], show_alert=True)
     is_non_admin = await is_nonadmin_chat(CallbackQuery.message.chat.id)
     if not is_non_admin:
         if CallbackQuery.from_user.id not in SUDOERS:
             admins = adminlist.get(CallbackQuery.message.chat.id)
             if not admins:
-                return await CallbackQuery.answer(_["admin_13"], show_alert=True)
+                return await CallbackQuery.answer(_["admin_18"], show_alert=True)
             else:
                 if CallbackQuery.from_user.id not in admins:
-                    return await CallbackQuery.answer(_["admin_14"], show_alert=True)
+                    return await CallbackQuery.answer(_["admin_19"], show_alert=True)
     playing = db.get(chat_id)
     if not playing:
         return await CallbackQuery.answer(_["queue_2"], show_alert=True)
     duration_seconds = int(playing[0]["seconds"])
     if duration_seconds == 0:
-        return await CallbackQuery.answer(_["admin_27"], show_alert=True)
+        return await CallbackQuery.answer(_["admin_35"], show_alert=True)
     file_path = playing[0]["file"]
     if "downloads" not in file_path:
-        return await CallbackQuery.answer(_["admin_27"], show_alert=True)
+        return await CallbackQuery.answer(_["admin_35"], show_alert=True)
     checkspeed = (playing[0]).get("speed")
     if checkspeed:
         if str(checkspeed) == str(speed):
             if str(speed) == str("1.0"):
                 return await CallbackQuery.answer(
-                    _["admin_29"],
+                    _["admin_37"],
                     show_alert=True,
                 )
     else:
         if str(speed) == str("1.0"):
             return await CallbackQuery.answer(
-                _["admin_29"],
+                _["admin_37"],
                 show_alert=True,
             )
     if chat_id in checker:
         return await CallbackQuery.answer(
-            _["admin_30"],
+            _["admin_38"],
             show_alert=True,
         )
     else:
         checker.append(chat_id)
     try:
         await CallbackQuery.answer(
-            _["admin_31"],
+            _["admin_39"],
         )
     except:
         pass
     mystic = await CallbackQuery.edit_message_text(
-        text=_["admin_32"].format(CallbackQuery.from_user.mention),
+        text=_["admin_40"].format(CallbackQuery.from_user.mention),
     )
     try:
-        await Yuki.speedup_stream(
+        await Yukki.speedup_stream(
             chat_id,
             file_path,
             speed,
@@ -103,10 +103,10 @@ async def del_back_playlist(client, CallbackQuery, _):
     except:
         if chat_id in checker:
             checker.remove(chat_id)
-        return await mystic.edit_text(_["admin_33"], reply_markup=close_markup(_))
+        return await mystic.edit_text(_["admin_41"], reply_markup=close_markup(_))
     if chat_id in checker:
         checker.remove(chat_id)
     await mystic.edit_text(
-        text=_["admin_34"].format(speed, CallbackQuery.from_user.mention),
+        text=_["admin_42"].format(speed, CallbackQuery.from_user.mention),
         reply_markup=close_markup(_),
     )
