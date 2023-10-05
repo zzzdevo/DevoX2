eb7f394d98dfd6350fc2a1b99abe2b37a3e9d76c
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-10-05 17:39:02 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,5 +1,3 @@
-
-
 import re
 import sys
 from os import getenv
@@ -342,10 +340,3 @@ if TELEGRAM_VIDEO_URL:
                 "[ERROR] - Your TELEGRAM_VIDEO_URL url is wrong. Please ensure that it starts with https://"
             )
             sys.exit()
-
-
-if not MUSIC_BOT_NAME.isascii():
-    print(
-        "[ERROR] - You've defined MUSIC_BOT_NAME wrong. Please don't use any special characters or Special font for this... Keep it simple and small."
-    )
-    sys.exit()
