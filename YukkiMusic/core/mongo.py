a288b081f8d5ffd6fb6ae1879c832123c1841c4c
zzzdevo <110237161+zzzdevo@users.noreply.github.com>
2023-10-05 09:31:30 +0300

    Update mongo.py

--- YukkiMusic/core/mongo.py
+++ YukkiMusic/core/mongo.py
@@ -15,7 +15,7 @@ import config
 
 from ..logging import LOGGER
 
-TEMP_MONGODB = "mongodb+srv://shikhar:shikhar@cluster0.6xzlh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+TEMP_MONGODB = config.MONGO_DB_URI
 
 
 if config.MONGO_DB_URI is None:
